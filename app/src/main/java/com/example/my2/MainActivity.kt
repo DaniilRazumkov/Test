9b1cfa6bfd976b4817ac5d56b8ed5e658a85e094
Daniil <daniilrazumkov9@gmail.com>
2021-12-24 10:53:16 +0500

    Test

---
+++ app/src/main/java/com/example/my2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.my2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.my2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityMainBinding
+
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+        bindingClass =
+        setContentView(bindingClass.)
+
+
+    }
+}
